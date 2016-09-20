bb61f6b0304d82c596b0d88f2b37d3f48264ed86
Vershinin Denis <kras_den@rambler.ru>
2016-09-20 23:44:20 +0700

    добавлен первый пример

---
+++ examples/01-hello-world/hello-world.c
@@ -0,0 +1,16 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int
+init_module(void)
+{
+	printk(KERN_INFO "Hello, World!!!\n");
+
+	return 0;
+}
+
+void
+cleanup_module(void)
+{
+	printk(KERN_INFO "Goodbuy, World!!!\n");
+}
